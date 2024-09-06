35ccf10e9c8d68fc841c74d4b0daccc5c54d9c06
Harsh <harshkrana7017@gmail.com>
2024-09-06 07:37:02 +0530

    added calls basic crud apis

--- Models.py
+++ Models.py
@@ -1,5 +1,15 @@
 from db.base import Base
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Date
+from sqlalchemy.ext.hybrid import hybrid_property
+from typing import List
+import enum
+
+class CallStatus(enum.Enum):
+    ended="Ended"
+    live="Live"
+    scheduled="Scheduled"
+
+    
 
 class Users(Base):
     __tablename__="users"
@@ -8,3 +18,32 @@ class Users(Base):
     username=Column(String, unique=True, index=True, nullable=False)
     email=Column(String, unique=True, index=True, nullable=False)
     hashed_password=Column(String, nullable=False)
+
+
+class Calls(Base):
+    __tablename__="calls"
+
+    call_id=Column(Integer, primary_key=True, index=True)
+    host_id=Column(Integer, nullable=False)
+    scheduled_at=Column(Date, nullable=True)
+    started_at=Column(Date, nullable=False)
+    ended_at=Column(Date, nullable=True)
+    call_members=Column(List[Integer], nullable=False)
+    is_call_private=Column(bool, nullable=True)
+
+
+    @hybrid_property
+    def call_duration(self):
+        return self.ended_at - self.started_at
+    
+    @hybrid_property
+    def call_status(self):
+        if self.ended_at:
+            return CallStatus.ended
+        elif self.started_at and not self.ended_at:
+            return CallStatus.live
+        elif self.scheduled_at and not self.started_at:
+            return CallStatus.scheduled
+        
+
+
---
+++ routers/calls.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, Depends
+from main import get_current_user
+from sqlalchemy.orm import Session
+from db.database import get_db
+from ..Models import Calls, Users
+from typing import Date
+
+router = APIRouter()
+
+@router.post("/call")
+def create_call(
+    scheduled_at:Date,
+    is_private_call:bool,
+    db:Session=Depends(get_db),
+    user:Users=Depends(get_current_user)
+    ):
+    len =len(db.query(Calls).all())
+    new_call ={}
+    new_call["host_id"]=user.id
+    new_call["call_members"]=[user.id]
+    new_call["call_id"]=len+1
+    new_call["scheduled_at"]=scheduled_at
+    new_call["is_private_call"]=is_private_call
+    db.add(Calls(**new_call))
+    db.commit()
+    return {"message":"Call Scheduled Sucessfully"}
+
+    
+    
+
+
+
+
+@router.get("/hosted/calls")
+def get_hosted_calls(db:Session=Depends(get_db),user:Users=Depends(get_current_user)):
+    return db.query(Calls).filter(Calls.host_id == user.id).all()
+
+@router.get("/member/calls")
+def get_member_calls(db:Session = Depends(get_db), user:Users=Depends(get_current_user)):
+    return db.query(Calls).filter(user.id in Calls.call_members).all()
+
+
+
+
+
