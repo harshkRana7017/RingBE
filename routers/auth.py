d17f61cfa5b7e79929b8c84189310ff4ad29345e
Harsh <harshkrana7017@gmail.com>
2024-09-07 08:20:43 +0530

    start call and code refactoring

---
+++ routers/auth.py
@@ -0,0 +1,89 @@
+from fastapi import APIRouter, Depends, HTTPException, Request, status
+from pydantic import BaseModel
+from requests import Session
+import requests
+from sqlalchemy import inspect
+
+from Models import Users
+from Schemas.user import LoginUser, SignUpUser
+from db.database import get_db
+from hashing import hashPassword, verify_password
+from tokenfuncs import create_access_token
+
+router = APIRouter()
+
+GOOGLE_CLIENT_ID ='754188762868-ak6bqkjap4jkldoof6oqu5doe2u090t0.apps.googleusercontent.com'
+class TokenRequest(BaseModel):
+    token: str
+    
+@router.post('/auth/google')
+def get_token(token_request: TokenRequest, db:Session = Depends(get_db)):
+    try:
+        
+        response= requests.get(f"https://oauth2.googleapis.com/tokeninfo?id_token={token_request.token}")
+        id_info=response.json()
+
+        if id_info['aud'] != GOOGLE_CLIENT_ID:
+            raise HTTPException(status_code=400, detail="Invalid Google Client ID")
+        user = db.query(Users).filter(Users.email == id_info['email']).first()
+        id =len(db.query(Users).all()) +1
+        if user is None:
+            user=Users(
+                email=id_info['email'],
+                username=id_info['name'],
+                hashed_password="",  # No password for Google sign-in,
+                id=id
+            )
+            db.add(user)
+            db.commit()
+            db.refresh(user)
+        user_dict = {
+            c.key:getattr(user, c.key)  
+            for c in inspect(user).mapper.column_attrs 
+            if c.key !='hashed_password'
+            }
+        access_token = create_access_token(data={"sub": user.email})
+        return {"token": access_token, "user":user_dict}
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid token or Google authentication failed"
+        )
+
+
+@router.get("/")
+def read_route(request: Request):
+    return {"message": "Hello World"}
+
+@router.post("/signup")
+def signup(request:Request, userData: SignUpUser, db:Session=Depends(get_db)):
+    new_user = Users(username=userData.username, email=userData.email, hashed_password=hashPassword(userData.password))
+    db.add(new_user)
+    db.commit()
+    return {"message":"User created Sucessfully"}
+
+@router.get("/users")
+def get_users(request:Request, db:Session=Depends(get_db)):
+   users = db.query(Users).all()
+   return users
+
+
+@router.post("/login")
+def get_token(request:Request, userData:LoginUser, db:Session = Depends(get_db)):
+     user =db.query(Users).filter(Users.email == userData.email).first()
+     if user:
+        if  verify_password(userData.password, user.hashed_password):
+           token= create_access_token(data={"sub": user.email})
+           return {"user":{"username":user.username, "id":user.id, "email":user.email},"token": token}
+        else:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect Passwor")
+     else:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User Not Found")
+     
+
+     
+
+
+
+          
+   
--- routers/calls.py
+++ routers/calls.py
@@ -1,24 +1,28 @@
 from fastapi import APIRouter, Depends
-from main import get_current_user
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from db.database import get_db
-from ..Models import Calls, Users
+from utils.get_user import get_current_user
+from Models import Calls, Users
 from datetime import datetime
 
 router = APIRouter()
 
+class CallCreateRequest(BaseModel):
+    is_call_private: bool
+
 @router.post("/call")
 def create_call(
-    is_private_call:bool,
+    call_data:CallCreateRequest,
     db:Session=Depends(get_db),
     user:Users=Depends(get_current_user)
     ):
-    len =len(db.query(Calls).all())
+    len =db.query(Calls).count()
     new_call ={}
     new_call["host_id"]=user.id
-    new_call["call_members"]=[user.id]
+    # new_call["call_members"]=[user.id]
     new_call["call_id"]=len+1
-    new_call["is_private_call"]=is_private_call
+    new_call["is_call_private"]=call_data.is_call_private
     new_call["started_at"]=datetime.now()
     db.add(Calls(**new_call))
     db.commit()
