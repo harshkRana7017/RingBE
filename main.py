d55ed18ed8b7202063e22ca9827a7032e5247cc5
Harsh <harshkrana7017@gmail.com>
2024-10-08 06:50:27 +0530

    basic code added

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+import json
+from typing import Dict
+from fastapi import Depends, FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware 
+from Models import Users
 from routers import calls, auth
+from utils.get_user import get_current_user
 
 app = FastAPI()
 
@@ -16,6 +20,49 @@ app.add_middleware(CORSMiddleware,
                    allow_methods=["*"],
                    allow_headers=["*"]
                    )
+
+class ConnectionManger :
+    def __init__(self):
+        self.active_connections: Dict[str, WebSocket]={}
+
+        async def connect(self, websocket:WebSocket, client_id:str):
+            await websocket.accept()
+            self.active_connections[client_id]=websocket
+
+        async def disconnect(self,websocket:WebSocket, client_id:str):
+            await websocket.close()
+            self.active_connections[client_id].close()
+            del self.active_connections[client_id]
+
+        async def send_message(self,message: str, client_id:str):
+            websocket: WebSocket= self.active_connections[client_id]
+            if websocket:
+                await websocket.send_text(message)
+
+        async def broadcast(self, message:str):
+            for websocket in self.active_connections.values():
+                await websocket.send_text(message)
+
+manager = ConnectionManger()
+
+
+
+@app.websocket("/ws")
+def websocket_endpoint(websocket:WebSocket,  user:Users= Depends(get_current_user)):
+    client_id = user.id
+    manager.connect(websocket, client_id)
+    try:
+        while True:
+            data =  websocket.receive_text()
+            message =json.loads(data)
+            target_client_id = message.get("target_client_id")
+            if target_client_id:
+                manager.send_message(json.dump(message), target_client_id)
+
+    except WebSocketDisconnect:
+        manager.disconnect(websocket, client_id)
+        
+
 
 app.include_router(calls.router)
 app.include_router(auth.router)
